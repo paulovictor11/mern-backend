a22fdf7bfe719ebae01cd95b68b0416d8acd46e7
paulovictor11 <pauloalmeidajp652@gmail.com>
2022-07-23 13:38:50 -0300

    signing up & hashing passwords

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -18,5 +19,20 @@ const userSchema = new Schema(
         timestamps: true,
     }
 );
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({ email });
+    if (exists) {
+        throw new Error("Email already in use");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ email, password: hash });
+
+    return user;
+};
 
 export default mongoose.model("User", userSchema);
